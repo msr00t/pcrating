7a8f294845e402e00dd768ba96913c93be1aca20
Sergio Tapia Gutierrez <sergiotapia@outlook.com>
2015-07-02 20:27:11 -0400

    Don't render images or links in markdown to prevent abuse.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def markdown(text)
-  	renderer = Redcarpet::Render::HTML.new(filter_html: true)
+  	renderer = Redcarpet::Render::HTML.new(filter_html: true, no_links: true, no_images: true)
   	markdown = Redcarpet::Markdown.new(renderer, extensions = {})
   	markdown.render(text).html_safe
   end
