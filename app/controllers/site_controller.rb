b4ca02b7ee7fbcec5caf47cc0fc77ebf8eec2c46
Daniel Lockhart <daniel@lockyy.com>
2015-07-15 23:14:47 +0100

    Improve how the slides on the landing page work

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,9 +1,30 @@
 class SiteController < ApplicationController
 
+  before_action :game_slides
+
   def index
-    @top_games = Game.top[0..1]
-    @games = Game.rated
     flash[:alert] = ENV['LANDING_NOTICE'] unless flash[:alert]
   end
+
+  private
+
+    def game_slides
+      games = Game.rated
+
+      @game_slides = [
+        {
+          title: 'Latest Games',
+          games: games.latest[0..1]
+        },
+        {
+          title: 'Top Games',
+          games: games.top[0..1]
+        },
+        {
+          title: 'Worst Games',
+          games: games.bottom[0..1]
+        },
+      ]
+    end
 
 end
