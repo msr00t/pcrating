ca6bf670e96d483ffac39e262177c762b83ad6fa
Daniel Lockhart <daniel@lockyy.com>
2015-07-29 23:53:54 +0100

    Adjust migrations

--- db/migrate/20150719225834_create_publishers.rb
+++ db/migrate/20150719225834_create_publishers.rb
@@ -2,8 +2,11 @@ class CreatePublishers < ActiveRecord::Migration
   def change
     create_table :publishers do |t|
       t.string :name
+      t.string :slug, unique: true
 
       t.timestamps null: false
     end
+
+    add_index :developers, :slug, unique: true
   end
 end
--- db/migrate/20150719225932_create_developers.rb
+++ db/migrate/20150719225932_create_developers.rb
@@ -2,8 +2,11 @@ class CreateDevelopers < ActiveRecord::Migration
   def change
     create_table :developers do |t|
       t.string :name
+      t.string :slug, unique: true
 
       t.timestamps null: false
     end
+
+    add_index :developers, :slug, unique: true
   end
 end
