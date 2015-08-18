8c4a9ad5fbe6fc74a5d5851766d492fc9ca57007
Daniel Lockhart <daniel@lockyy.com>
2015-08-18 19:35:05 +0100

    Fix a faulty migration

--- db/migrate/20150717202401_create_reviews.rb
+++ db/migrate/20150717202401_create_reviews.rb
@@ -44,16 +44,16 @@ class CreateReviews < ActiveRecord::Migration
 
       t.timestamps null: false
 
-      t.integer :reviews, :cached_votes_total, default: 0
-      t.integer :reviews, :cached_votes_score, default: 0
-      t.integer :reviews, :cached_votes_up, default: 0
-      t.integer :reviews, :cached_votes_down, default: 0
-      t.integer :reviews, :cached_weighted_score, default: 0
-      t.integer :reviews, :cached_weighted_total, default: 0
-      t.float :reviews, :cached_weighted_average, default: 0.0
+      t.integer :cached_votes_total, default: 0
+      t.integer :cached_votes_score, default: 0
+      t.integer :cached_votes_up, default: 0
+      t.integer :cached_votes_down, default: 0
+      t.integer :cached_weighted_score, default: 0
+      t.integer :cached_weighted_total, default: 0
+      t.float   :cached_weighted_average, default: 0.0
 
-      t.float :reviews, :cached_rank
-      t.float :reviews, :cached_score
+      t.float :cached_rank
+      t.float :cached_score
 
     end
 
