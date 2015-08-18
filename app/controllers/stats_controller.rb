0ad36436752c3b1ec9e620ad3c3519000795a474
Daniel Lockhart <daniel@lockyy.com>
2015-08-18 14:35:49 +0100

    Add charts for ranks

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,6 +2,7 @@ class StatsController < ApplicationController
 
   def stats
     @game_scores = Game.all.map(&:cached_score).inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.sort.to_h.to_a
+    @game_ranks = Game.all.map(&:cached_rank).inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.sort.to_h.to_a
   end
 
 end
