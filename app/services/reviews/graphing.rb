b1af1460145cea7aa07ceb2ae27af285de858587
Daniel Lockhart <daniel@lockyy.com>
2015-08-20 22:17:38 +0100

    Make Reviews::Graphing.get_grouped_counts return accumulated users rather than just the signups for each day

--- app/services/reviews/graphing.rb
+++ app/services/reviews/graphing.rb
@@ -33,8 +33,26 @@ module Reviews
 
     private
 
+      # TODO: Refactor this
       def self.get_grouped_counts(model)
-        result = model.group_by_day(:created_at, format: "%d %m %y").count.to_a.to_s
+        # Go through all records for the given model and group them by the day they were created
+        # Turn that hash of day => count into an array of [day, count]
+        grouped_by_day = model.group_by_day(:created_at, format: '%d %m %y').count.to_a
+
+        # Here are are creating the array that looks like this
+        # [[day - n, total_count_up_to_n], [day - (n+1), total_count_up_to_n+1] ... [day, total_count_up_to_day]]
+        # or in a real example [['3rd august', 1], ['4th august', 4], ['5th august', 5]], where the total users on
+        # the 5th is 5.
+        # To do that we are using inject on the 2d array created above.
+        # Each time around the loop we create a new row for result_array that consists of a date, along with
+        # the last inserted row's total + the total for the date we're currently working on.
+        grouped_by_day.inject([['Start', 0]]) do |result_array, day_count|
+          date = day_count[0]
+          total_on_date = day_count[1]
+
+          new_row = [date, result_array.last[1] + total_on_date]
+          result_array << new_row
+        end
       end
 
   end
