4766f610332de9e46c64d5c7dfc8712a4739850a
Daniel Lockhart <daniel@lockyy.com>
2015-08-27 21:35:27 +0100

    Add GMG game lookup to game creation

--- app/services/affiliates/parsing.rb
+++ app/services/affiliates/parsing.rb
@@ -2,17 +2,26 @@ module Affiliates
   # Goes through the xml/json from GMG and gathers important information
   class Parsing
 
-    def self.parse
+    def self.parse_all
       found = 0
       GMG_DATA.each do |gmg_game|
         game = Game.find_by(title: gmg_game[:name])
         if game
           found += 1
-          game.gmg_url = URI.escape("#{ENV['GMG_AFFILIATE_LINK']}?url=#{gmg_game[:url]}")
+          game.gmg_url = self.create_url(gmg_game[:url])
           game.save
         end
       end
       return found
+    end
+
+    def self.get_url_for_game(game)
+      gmg_game = GMG_DATA.select { |gmg_game| gmg_game[:name] == game.title }
+      return self.create_url(gmg_game[:url])
+    end
+
+    def self.create_url(url)
+      URI.escape("#{ENV['GMG_AFFILIATE_LINK']}?url=#{url}")
     end
 
   end
