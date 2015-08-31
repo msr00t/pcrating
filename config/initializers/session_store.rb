40e65949e55e69381901be47604ce27d80466a8a
Daniel Lockhart <daniel@lockyy.com>
2015-08-31 19:05:49 +0100

    pcmrating -> pcrating

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pcmrating_session'
+Rails.application.config.session_store :cookie_store, key: '_pcrating_session'
