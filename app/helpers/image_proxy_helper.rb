7de60d620d4f7ea554e04f45cba30d2b57ee2d6e
Daniel Lockhart <daniel@lockyy.com>
2015-08-31 21:07:03 +0100

    Fix for proxy images

--- app/helpers/image_proxy_helper.rb
+++ app/helpers/image_proxy_helper.rb
@@ -1,7 +1,7 @@
 require 'base64'
 
 module ImageProxyHelper
-  def proxy_image_tag source, options = nil
+  def proxy_image_tag source, options = {}
     options[:src] = "/image_proxy/#{CGI::escape(Base64.encode64(source))}"
     tag("img", options)
   end
